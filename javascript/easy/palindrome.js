83998f4b993339975928999ad0eb40f3ecb69f85
jasur <tarancci@gmail.com>
2021-07-31 15:59:56 -0400

    fix: solved palindrome algo with JavaScript

---
+++ javascript/easy/palindrome.js
@@ -0,0 +1,23 @@
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+// const palindrome = (string) => {
+//     // reverse the string into a const called reversedString
+//     // directly compare string and reversedString and return the result
+//     const reversedString = string.split('').reverse().join('');
+//     return string === reversedString;
+// };
+
+const palindrome = (string) => {
+    // turn string into array
+    // use .every() helper method on array and return the result
+    return string.split('').every((character, index) => character === string[string.length - index - 1]);
+};
+
+module.exports = palindrome;
